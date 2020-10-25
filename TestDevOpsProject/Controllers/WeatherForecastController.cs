82b8b952fba4a3f8a387882a68981ace1f6322fb
Nirmallya <nirmallya2013@live.com>
2020-10-25 16:04:53 +0800

    complete for 200

--- TestDevOpsProject/Controllers/WeatherForecastController.cs
+++ TestDevOpsProject/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace TestDevOpsProject.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Feature 200"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
